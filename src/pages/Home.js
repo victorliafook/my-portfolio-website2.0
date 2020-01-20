516df9dde8955cfaf2de87ab46a1f8c6c95792b6
Victor Lia Fook <victorliafook@gmail.com>
2020-01-20 09:22:51 +0000

    updating home text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,11 +14,11 @@ const Home = () => (
                 <h1 className="pb-2 mt-4 mb-2 border-bottom">It's nice to meet you!</h1>
             </Col>
             <Col lg="12">
-                <p>My name is Victor Lia Fook, I am a Software Engineer from Brazil, making a living in the beautiful Australia! Please feel free to take a look at this website, and learn more about me as well as my work and passion: Computer Programming!</p>
+                <p>My name is Victor Lia Fook, I am a Software Engineer from Brazil, making a living in the beautiful Australia! Please feel free to take a look at this website, and learn more about me and my work and passion: Computer Programming!</p>
 
                 <p>The main goal of this website is to serve as a simple portfolio of my skills and work as a programmer.</p>
 
-                <p>However, I also want this to be a place to share some of my experience with the community - Some code, techniques and tips. Sure, I know that there are plenty of specialized blogs and websites that certainly do it better than me but I believe that knowledge is to be shared. So, I hope you enjoy it!</p>
+                <p>However, I also want this to be a place to share some of my experience and personal projects - including code, programming techniques and tips. After all, knowledge is to be shared. So, I hope you enjoy it!</p>
             </Col>
         </Row>
 
