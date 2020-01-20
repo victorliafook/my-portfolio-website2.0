bd7a6ee68fa1d50ffffcceced2236d89c5d3e5fc
Victor Lia Fook <victorliafook@gmail.com>
2020-01-20 09:04:18 +0000

    adding header styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,12 +10,12 @@ const Home = () => (
     <Container>
         <Row>
             <Col lg="12">
-                <h1>It's nice to meet you!</h1>
+                <h1 className="pb-2 mt-4 mb-2 border-bottom">It's nice to meet you!</h1>
             </Col>
             <Col lg="12">
-                <p>My name is Victor Lia Fook, I am a Software & Web Developer from Brazil, making a living in the beautiful Australia! Please feel free to take a look at this website, and learn more about me as well as my work and passion: Computer Programming!</p>
+                <p>My name is Victor Lia Fook, I am a Software Engineer from Brazil, making a living in the beautiful Australia! Please feel free to take a look at this website, and learn more about me as well as my work and passion: Computer Programming!</p>
 
-                <p>The main goal of this website is to serve as a fast portfolio of my skills and work as a programmer. By the way, I can help you make your web project on a freelance or contract basis.</p>
+                <p>The main goal of this website is to serve as a simple portfolio of my skills and work as a programmer.</p>
 
                 <p>However, I also want this to be a place to share some of my experience with the community - Some code, techniques and tips. Sure, I know that there are plenty of specialized blogs and websites that certainly do it better than me but I believe that knowledge is to be shared. So, I hope you enjoy it!</p>
             </Col>
