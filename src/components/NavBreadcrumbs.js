d9ca102189aea660021c2c632dfcca1df7857f7a
Victor Lia Fook <victorliafook@gmail.com>
2020-01-23 06:24:05 +0000

    silly breadcrumb bug fix

--- src/components/NavBreadcrumbs.js
+++ src/components/NavBreadcrumbs.js
@@ -3,9 +3,10 @@ import { Breadcrumb } from  'react-bootstrap';
 
 const NavBreadcrumbs = ({ hierarchyList }) => (
   <Breadcrumb>
-    <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+    <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
     {hierarchyList.map((item, key) => (
       <Breadcrumb.Item 
+        key={key}
         href={item.href}
         active={item.active}>{item.name}
       </Breadcrumb.Item>
